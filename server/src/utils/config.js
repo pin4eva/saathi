090d50c6cef5757edd3bdddd72f13707c1f81390
Peter Akaliro <pin4eva@hotmail.com>
2021-03-29 10:54:36 +0100

    hdhd

--- server/src/utils/config.js
+++ server/src/utils/config.js
@@ -11,7 +11,7 @@ export const AWS_CONFIG = {
   },
   aws_remote_config: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_KEY,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     region: "us-east-1",
   },
 };
